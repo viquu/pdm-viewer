f8362e2e94e14930da795b4c00efae091502938a
viquu <viquuu@gmail.com>
2018-11-28 16:07:04 +0800

    将win.show()过时的使用方法必为使用win.setVisible(true)

--- src/main/java/PDMViewer.java
+++ src/main/java/PDMViewer.java
@@ -12,7 +12,7 @@ public class PDMViewer {
 
 	public static void main(String[] args) {
 		MainWindow jwin = new MainWindow();
-		jwin.show();
+		jwin.setVisible(true);
 		if (args.length > 1 && args[1] != null) {
 			File f = new File(args[1]);
 			if (f.exists() && f.isFile()) {
